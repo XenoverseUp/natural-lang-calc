107df6deebe32436cc7ff8608d0d0d34ba05714e
XenoverseUp <xenoverseup@gmail.com>
2025-08-25 15:03:54 +0300

    init: let there be code

---
+++ src/main/java/com/xenoverseup/natural_calculator/NaturalCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.xenoverseup.natural_calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaturalCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaturalCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xenoverseup/natural_calculator/NaturalCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xenoverseup.natural_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaturalCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
