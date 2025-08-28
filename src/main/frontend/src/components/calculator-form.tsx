2b59b74b1da8807315a5e8476e7fbe7e950cdca8
XenoverseUp <xenoverseup@gmail.com>
2025-08-29 02:14:53 +0300

    feat: define form schema

--- src/main/frontend/src/components/calculator-form.tsx
+++ src/main/frontend/src/components/calculator-form.tsx
@@ -1,24 +1,54 @@
 import type { ClassValue } from "clsx";
 import { Form } from "./ui/form";
-import { cn } from "@/lib/utils";
+import { cn, isValidNumberWord } from "@/lib/utils";
 import { useForm } from "react-hook-form";
-import InputGroup from "./ui/input-group";
-import { Textarea } from "./ui/textarea";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { useTranslation } from "react-i18next";
 
 interface Props {
   className?: ClassValue;
 }
 
 export default function CalculatorForm({ className }: Props) {
-  const form = useForm();
+  const { t, i18n } = useTranslation();
+  const currentLanguage = i18n.language as "en" | "tr";
+
+  const formSchema = z.object({
+    firstNumber: z
+      .string()
+      .transform((val) => val.replace(/\s+/g, " ").trim())
+      .refine((val) => val.length >= 3, { message: "Must be at least 3 characters" })
+      .refine((val) => isValidNumberWord(val, currentLanguage), {
+        message: "Must be a valid number in words",
+      }),
+    secondNumber: z
+      .string()
+      .transform((val) => val.replace(/\s+/g, " ").trim())
+      .refine((val) => val.length >= 3, { message: "Must be at least 3 characters" })
+      .refine((val) => isValidNumberWord(val, currentLanguage), {
+        message: "Must be a valid number in words",
+      }),
+    operation: z.enum(["ADD", "SUBTRACT", "MULTIPLY", "DIVIDE", "MOD"]),
+  });
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      operation: "ADD",
+      firstNumber: "",
+      secondNumber: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
 
   return (
     <Form {...form}>
-      <form className={cn(className, "px-6")}>
-        <InputGroup>
-          <Textarea placeholder="First Number" className="font-mono" />
-          <Textarea placeholder="Second Number" className="font-mono" />
-        </InputGroup>
+      <form onSubmit={form.handleSubmit(onSubmit)} className={cn(className, "px-6")}>
+        Form.
       </form>
     </Form>
   );
--- src/main/frontend/src/lib/utils.ts
+++ src/main/frontend/src/lib/utils.ts
@@ -1,5 +1,6 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+import grammar from "@/lib/localization/grammar.json";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -7,4 +8,12 @@ export function cn(...inputs: ClassValue[]) {
 
 export function getClientLocale() {
   return (navigator.language || navigator.languages[0] || "en-US").split("-").at(0)!;
+}
+
+export function isValidNumberWord(val: string, language: "en" | "tr"): boolean {
+  const words = grammar[language] || grammar.en;
+  const pattern = `^(${words.join("|")})(\\s+(${words.join("|")}))*$`;
+  const regex = new RegExp(pattern, "i"); // case insensitive
+
+  return regex.test(val);
 }
